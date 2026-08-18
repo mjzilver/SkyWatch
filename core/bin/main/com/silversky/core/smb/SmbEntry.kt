29e204bd9156d68b9d80048fcc59c4b455600fbf
Jari <4ilverj@gmail.com>
2026-08-18 23:47:06 +0200

    Remove bin

--- core/bin/main/com/silversky/core/smb/SmbEntry.kt
+++
@@ -1,7 +0,0 @@
-package com.silversky.core.smb
-
-data class SmbEntry(
-    val name: String,
-    val isDirectory: Boolean,
-    val path: String,
-)
--- core/bin/main/com/silversky/core/smb/SmbServer.kt
+++
@@ -1,7 +0,0 @@
-package com.silversky.core.smb
-
-data class SmbServer(
-    var name: String?,
-    val ipAddress: String,
-    val port: Int = 445,
-)
