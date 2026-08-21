25fe5965a740752b9a870964cc2baf8feb2131b2
Jari <4ilverj@gmail.com>
2026-08-21 11:04:13 +0200

    Update versions

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -11,8 +11,8 @@ java {
 kotlin { compilerOptions { jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11 } }
 
 dependencies {
-  implementation("com.hierynomus:smbj:0.14.0")
-  implementation("com.rapid7.client:dcerpc:0.12.13")
+  implementation(libs.smbj)
+  implementation(libs.dcerpc)
   implementation("eu.agno3.jcifs:jcifs-ng:2.1.9") {
     exclude(
         group = "org.bouncycastle",
